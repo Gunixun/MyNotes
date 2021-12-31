da4210fb5e9ba7933671d949f381f521cd84563f
GanievAR <ganievar@bnipi.rosneft.ru>
2021-12-31 11:33:45 +0500

    Рефакторинг

--- app/src/main/java/com/example/mynotes/ui/adapters/AdapterItem.java
+++ app/src/main/java/com/example/mynotes/ui/adapters/AdapterItem.java
@@ -1,4 +1,13 @@
 package com.example.mynotes.ui.adapters;
 
 public class AdapterItem {
+    private final String key;
+
+    public AdapterItem(String key){
+        this.key = key;
+    }
+
+    public String getKey(){
+        return key;
+    }
 }
--- app/src/main/java/com/example/mynotes/ui/adapters/NoteAdapterItem.java
+++ app/src/main/java/com/example/mynotes/ui/adapters/NoteAdapterItem.java
@@ -9,6 +9,7 @@ public class NoteAdapterItem extends AdapterItem {
     private final String date;
 
     public NoteAdapterItem(Note note, String title, String body, String date) {
+        super(note.getId());
         this.note = note;
         this.title = title;
         this.body = body;
@@ -29,5 +30,15 @@ public class NoteAdapterItem extends AdapterItem {
 
     public Note getNote() {
         return note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof NoteAdapterItem)) {
+            return false;
+        } else {
+            return getBody().equals(((NoteAdapterItem) o).getBody())
+                    && getTitle().equals(((NoteAdapterItem) o).getTitle());
+        }
     }
 }
